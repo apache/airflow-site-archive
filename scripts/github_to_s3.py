3698c844453dc80b8d630ab741f3c2931e114482
Jarek Potiuk <jarek@potiuk.com>
2025-05-09 22:23:30 +0200

    Small improvements in github to s3 workflow (#12)

--- scripts/github_to_s3.py
+++ scripts/github_to_s3.py
@@ -40,7 +40,7 @@ class GithubToS3(CommonTransferUtils):
         super().__init__(bucket, local_path)
 
     @staticmethod
-    def fetch_last_commit_files(commit_sha, diff_filter="ACM"):
+    def fetch_commit_files(commit_sha, diff_filter="ACM"):
         console.print(f"[blue] Fetching files from last commit {commit_sha} [/]")
         cmd = [
             "git",
@@ -61,7 +61,7 @@ class GithubToS3(CommonTransferUtils):
             return []
         return result.stdout.splitlines() if result.stdout else []
 
-    def sync_last_commit_files(self, commit_sha: str, processes: int):
+    def sync_single_commit_files(self, commit_sha: str, processes: int):
         '''
         There are two parts here.
         1. When any file gets removed under docs folder, we will remove from target location
@@ -70,8 +70,8 @@ class GithubToS3(CommonTransferUtils):
         # Fetching `d` excludes deleted files
         # Fetching `D` includes deleted files
 
-        files_cp_required = self.fetch_last_commit_files(commit_sha, diff_filter="d")
-        files_del_required = self.fetch_last_commit_files(commit_sha, diff_filter="D")
+        files_cp_required = self.fetch_commit_files(commit_sha, diff_filter="d")
+        files_del_required = self.fetch_commit_files(commit_sha, diff_filter="D")
 
         files_cp_required_under_docs = [f for f in files_cp_required if f.startswith("docs-archive/")]
         files_del_required_required_under_docs = [f for f in files_del_required if f.startswith("docs-archive/")]
@@ -102,15 +102,19 @@ class GithubToS3(CommonTransferUtils):
 
         self.run_with_pool(self.sync, pool_args, processes=processes)
 
+def convert_short_name_to_folder_name(short_name: str):
+    if not short_name.startswith("apache-airflow-providers-"):
+        return f"apache-airflow-providers-{short_name.replace('.', '-')}"
+    return short_name
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Sync GitHub to S3")
     parser.add_argument("--bucket-path", required=True, help="S3 bucket name with path")
     parser.add_argument("--local-path", required=True, help="local path to sync")
-    parser.add_argument("--document-folder", help="Document folder to sync", default="")
+    parser.add_argument("--document-folder", help="Document folder to sync (or short provider-id)", default="")
     parser.add_argument("--commit-sha", help="Commit SHA to sync", default="")
-    parser.add_argument("--sync-type", help="Sync type", default="last_commit")
+    parser.add_argument("--sync-type", help="Sync type", default="single_commit")
     parser.add_argument("--processes", help="Number of processes", type=int, default=8)
 
     args = parser.parse_args()
@@ -120,21 +124,23 @@ if __name__ == "__main__":
 
     document_folder = args.document_folder
 
-    if document_folder and document_folder != "NO_DOCS":
+    if document_folder and document_folder != "":
         full_local_path = Path(f"{args.local_path}/{document_folder}")
+        if not full_local_path.exists():
+            full_local_path = Path(f"{args.local_path}/{convert_short_name_to_folder_name(document_folder)}")
         if full_local_path.exists():
             console.print(f"[blue] Document folder {document_folder} exists in bucket {args.bucket_path}.[/]")
 
             destination = f"s3://{syncer.bucket_name}/{syncer.prefix}".rstrip("/") + "/" + document_folder
-            syncer.sync(source=full_local_path, destination=destination)
+            syncer.sync(source=full_local_path.as_posix(), destination=destination)
             sys.exit(0)
         else:
-            console.print(f"[red] Document folder {document_folder} does not exist in github {args.local_path}.[/]")
+            console.print(f"[red] Document folder {full_local_path} does not exist.[/]")
             sys.exit(1)
 
-    if args.sync_type == "last_commit" and args.commit_sha:
+    if args.sync_type == "single_commit" and args.commit_sha:
         console.print(f"[blue] Syncing last commit {args.commit_sha} from {args.local_path} [/]")
-        syncer.sync_last_commit_files(args.commit_sha, processes=int(args.processes))
+        syncer.sync_single_commit_files(args.commit_sha, processes=int(args.processes))
         sys.exit(0)
 
     if args.sync_type == "full_sync":
